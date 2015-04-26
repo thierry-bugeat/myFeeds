d1f3bec3417f914402bfeef98f0e258857f50678
Thierry BUGEAT <thierry@quiksilver.fr>
2015-04-26 11:14:25 +0200

    Update inline CSS code.

--- style/inline.css.js
+++ style/inline.css.js
@@ -1,3 +1,24 @@
 /* browser > main entry */
 
-var _srcDocCss = '<style>* {box-sizing: border-box; background-color: #FFFFFF; max-width: 100%; height: auto; overflow-x:hidden; } a {text-decoration:none;} html{margin:0; font-size: 62.5%; padding:0;} .entrie-title {font-size: 1.7rem; font-weight:bold;} .entrie-date {font-size:1.4rem; color:#FF4E00;} .entrie-author {border-bottom: 1px solid #5F5F5F} .entrie-feed-title {color:#00AACC;} .entrie-contentSnippet{font-size: 1.7rem; color:#5F5F5F;} a{color: #00AACC;}</style>';
+var _srcDocCss = [
+'<style>                                                            ',
+'*                                                                  ',
+'{                                                                  ',
+'   box-sizing: border-box;                                         ',
+'   background-color: #FFFFFF;                                      ',
+'   max-width: 100%;                                                ',
+'   height: auto;                                                   ',
+'   overflow-x: hidden;                                             ',
+'   margin: 0;                                                      ',
+'   padding: 0;                                                     ',
+'}                                                                  ',
+'body                   {padding: 2.5%;}                            ',
+'html                   {margin: 0; font-size: 62.5%; padding: 0;}  ',
+'.entrie-title          {font-size: 1.7rem; font-weight: bold;}     ',
+'.entrie-date           {font-size: 1.4rem; color: #FF4E00;}        ',
+'.entrie-author         {border-bottom: 1px solid #5F5F5F}          ',
+'.entrie-feed-title     {color: #00AACC;}                           ',
+'.entrie-contentSnippet {font-size: 1.7rem; color: #5F5F5F;}        ',
+'a                      {text-decoration: none; color: #00AACC;}    ',
+'</style>                                                           '
+].join(''); 
