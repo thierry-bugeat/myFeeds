759f1e5f4af8800b1a81deee3f57dab241545171
Thierry BUGEAT <thierry.bugeat@quiksilver.eu>
2015-08-28 13:35:08 +0200

    Add libs myCore & sha256

---
+++ libs/myCore.js
@@ -0,0 +1,15 @@
+Number.isInteger = Number.isInteger || function(value) {
+    return typeof value === "number" &&
+        isFinite(value) &&
+        Math.floor(value) === value;
+};
+
+Array.prototype.contains = function(obj) {
+    var i = this.length;
+    while (i--) {
+        if (this[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
