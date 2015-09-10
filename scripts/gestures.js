b4f8e85403f643694b8bb63585d4990ca7675fd1
Thierry BUGEAT <thierry.bugeat@quiksilver.eu>
2015-09-10 17:52:46 +0200

    Update gesture id

--- scripts/gestures.js
+++ scripts/gestures.js
@@ -63,7 +63,7 @@ function _swipe(callback) {
         
         // Sync : Launch a sync if no synchro is in progress
         
-        if ((myGesture._action == "sync") && (sync.style.pointerEvents == 'auto')) {
+        if ((myGesture._action == "sync") && (workerSync.style.pointerEvents == 'auto')) {
             ui._onclick(workerSync, 'disable');
             myWorker.postMessage({'cmd': 'loadFeeds', 'params': MyFeeds.params, 'myFeedsSubscriptions': myFeedsSubscriptions});
         }
