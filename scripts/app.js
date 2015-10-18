48beb3323878a1e4fe9c50e2b1fb8f0262ebbb84
Thierry BUGEAT <myfeeds@bugeat.com>
2015-10-18 09:19:57 +0200

    Display message AolReader.getSubscriptions.error only for developper

--- scripts/app.js
+++ scripts/app.js
@@ -2411,7 +2411,7 @@
 
         document.body.addEventListener('AolReader.getSubscriptions.error', function(response) {
             my.log('CustomEvent : AolReader.getSubscriptions.error', arguments);
-            my.message(document.webL10n.get('aolreader-get-subscriptions-error') + response.detail.message);
+            my.alert(document.webL10n.get('aolreader-get-subscriptions-error') + response.detail.message);
         });
 
         // ============
